6e6d5dc35df3fdcb33a545154b042fff0fc7989c
Devraj Singh <dvrjsngh169@gmail.com>
2026-04-10 17:41:40 +0530

    final changes

--- src/main/java/com/devraj/registration/RegistrationApplication.java
+++ src/main/java/com/devraj/registration/RegistrationApplication.java
@@ -2,16 +2,9 @@ package com.devraj.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RegistrationApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(RegistrationApplication.class);
-    }
+public class RegistrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistrationApplication.class, args);
