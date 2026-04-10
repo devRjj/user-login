09a502c56244b02b11ccf568bcde0605d94cb5f6
Devraj Singh <dvrjsngh169@gmail.com>
2026-04-10 14:07:08 +0530

    packagin war and tomcat added

--- src/main/java/com/devraj/registration/RegistrationApplication.java
+++ src/main/java/com/devraj/registration/RegistrationApplication.java
@@ -2,12 +2,17 @@ package com.devraj.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RegistrationApplication {
+public class RegistrationApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistrationApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RegistrationApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationApplication.class, args);
+    }
 }
