ed7ab0c8d0263130a8d1ed66fa8175adf6eab0c0
Devraj Singh <dvrjsngh169@gmail.com>
2026-04-10 18:57:20 +0530

    error controller issue

--- src/main/java/com/devraj/registration/controller/ErrorController.java
+++ src/main/java/com/devraj/registration/controller/ErrorController.java
@@ -1,6 +1,5 @@
 package com.devraj.registration.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,7 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class GlobalErrorController implements ErrorController {
+public class ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
